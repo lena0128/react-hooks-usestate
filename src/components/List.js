2ff7e931d86e68033834d0d67b1e9b2e020ad88d
Lena Hou <lenahou2013@gmail.com>
2022-01-05 11:29:54 -0500

    refactor List component using state hook

--- src/components/List.js
+++ src/components/List.js
@@ -1,41 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class List extends React.Component {
-    state = {
-        list: [],
-        item: ""
-    }
+function List(){
 
-    addToList = (e) => {
+    // state ={list: [], item: ""}
+    const [list, setList] = useState([])
+    const [item, setItem] = useState("")
+
+    function addToList(e){
         e.preventDefault()
-        if (this.state.item) {
-            this.setState(prevState => ({
-                list: [...prevState.list, prevState.item],
-                item: ""
-            }))
-        }
+
+        if(item) {
+           setList(prevList => [...prevList, item])
+           setItem("")
+        } 
+
     }
 
-    handleChange = (e) => {
-        this.setState({
-            item: e.target.value
-        })
+    function handleChange(e){
+        setItem(e.target.value)
     }
 
-    render() { 
-        return (
-            <div className="left">
-                <h3>Your List</h3>
-                <ul>
-                    {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
-                </ul>
-                <form onSubmit={this.addToList}>
-                    <input type="text" value={this.state.item} onChange={this.handleChange}/>
-                    <input type="submit" value="Add to List"/>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="left">
+            <h3>Your List</h3>
+            <ul>
+                {list.map((item, i) => <li key={i}>{item}</li>)}
+            </ul>
+            <form onSubmit={addToList}>
+                <input type="text" value={item} onChange={handleChange}/>
+                <input type="submit" value="Add to List"/>
+            </form>
+        </div>
+    );
+
 }
- 
-export default List;
+
+export default List
+
+// class List extends React.Component {
+//     state = {
+//         list: [],
+//         item: ""
+//     }
+// 
+//     addToList = (e) => {
+//         e.preventDefault()
+//         if (this.state.item) {
+//             this.setState(prevState => ({
+//                 list: [...prevState.list, prevState.item],
+//                 item: ""
+//             }))
+//         }
+//     }
+// 
+//     handleChange = (e) => {
+//         this.setState({
+//             item: e.target.value
+//         })
+//     }
+// 
+//     render() { 
+//         return (
+//             <div className="left">
+//                 <h3>Your List</h3>
+//                 <ul>
+//                     {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
+//                 </ul>
+//                 <form onSubmit={this.addToList}>
+//                     <input type="text" value={this.state.item} onChange={this.handleChange}/>
+//                     <input type="submit" value="Add to List"/>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+//  
+// export default List;
