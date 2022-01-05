c8f05aafb1e197e4fa7486606e37703c3b5eafe8
Lena Hou <lenahou2013@gmail.com>
2022-01-05 11:06:39 -0500

    refactor Main component using hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import List from './List';
 
-class Main extends React.Component {
-    state = {
-        darkMode: false
-    }
+function Main(){
+// class Main extends React.Component { 
 
-    handleModeChange = () => {
-        this.setState(prevState => ({
-            darkMode: !prevState.darkMode
-        }))
-    }
+    const [darkMode, setMode] = useState(false)
+    //     state = {
+    //         darkMode: false
+    //     }
 
-    render() {
-        return (
-            <div className={this.state.darkMode ? "main dark-mode" : "main light-mode"}>
-                <button onClick={this.handleModeChange}>{this.state.darkMode ? "Light Mode" : "Dark Mode"}</button>
-                <Counter />
-                <List />
-            </div>
-        )
+
+    function handleModeChange(){
+        setMode(prevMode => !prevMode)
     }
+    //     handleModeChange = () => {
+    //         this.setState(prevState => ({
+    //             darkMode: !prevState.darkMode
+    //         }))
+    //     }
+
+    return(
+        <div className={darkMode ? "main dark-mode" : "main light-mode"}>
+            <button onClick={handleModeChange}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
+            <Counter />
+            <List />
+        </div>
+    )
+
 }
 
-export default Main;
+export default Main
+
+
+// 
+//     render() {
+//         return (
+//             <div className={this.state.darkMode ? "main dark-mode" : "main light-mode"}>
+//                 <button onClick={this.handleModeChange}>{this.state.darkMode ? "Light Mode" : "Dark Mode"}</button>
+//                 <Counter />
+//                 <List />
+//             </div>
+//         )
+//     }
+// }
+// 
+// export default Main;
